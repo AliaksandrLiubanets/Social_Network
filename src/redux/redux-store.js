3e206406b20e69ee8dcfa76eee97ec1861cd0b5c
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-04-27 20:11:44 +0300

    Import thunk-redux

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,21 +1,21 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {combineReducers} from 'redux';
 import reducerAuth from './reducer-auth';
 import reducerDialogs from './reducer-dialogs';
 import reducerProfile from './reducer-profile';
 import reducerSidebar from './reducer-sidebar';
 import reducerUsers from './reducer-users';
+import thunkMiddleWare from 'redux-thunk'
 
 let reducers = combineReducers({
     profilePage: reducerProfile,
     dialogsPage: reducerDialogs,
     sidebar: reducerSidebar,
     usersPage: reducerUsers,
-    auth: reducerAuth,
-    
+    auth: reducerAuth,    
 })
 
-let store = createStore(reducers);
+let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
 // getState();
 // subscribe(observer);
 // dispatch(action);
