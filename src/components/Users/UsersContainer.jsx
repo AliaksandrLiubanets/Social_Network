300e5aa82037f84dea1fadc0d48d97aef7a1ecb1
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-05-11 16:35:19 +0300

    refactor comments in UsersContainer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -15,27 +15,12 @@ class UsersContainer extends React.Component {
 
         this.props.getUsersThunkCreator(this.props.currentPage, this.props.pageSize)
                 
-        //     this.props.toggleIsFetching(true)        
-        //     usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
-        //     .then(data => {                
-        //             this.props.toggleIsFetching(false) 
-        //             this.props.setUsers(data.items)
-        //             this.props.setTotalUsersCount(data.totalCount)                
-        //     })   
     }    
     
     onChangeClick = (pageNumber) => {
 
         this.props.getUsersThunkCreator(pageNumber, this.props.pageSize)
 
-        //     this.props.toggleIsFetching(true)
-        //     this.props.setCurrentPage(pageNumber);
-        //     // axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true}) :
-        //     usersAPI.getUsers(pageNumber, this.props.pageSize)
-        //     .then(data => {                 
-        //             this.props.toggleIsFetching(false)
-        //             this.props.setUsers(data.items)
-        //     })   
     }    
             
     render = () => {                    
@@ -68,59 +53,6 @@ const mapStateToProps = (state) => {
         followingInProgress: state.usersPage.followingInProgress,        
     }
 }
-
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         followUser: (userId) => {
-//             dispatch(followAC(userId))
-//         },
-//         unfollowUser: (userId) => {
-//             dispatch(unfollowAC(userId))
-//         },
-//         setUsers: (users) => {
-//             dispatch(setUsersAC(users))
-//         },
-//         setCurrentPage: (users) => {
-//             dispatch(setCurrentPageAC(users))
-//         },
-//         setTotalUsersCount: (usersCount) => {
-//             dispatch(setTotalUsersCountAC(usersCount))
-//         },
-//         toggleIsFetching: (isFetching) => {
-//             dispatch(toggleIsFetchingAC(isFetching))
-//         }
-
-//     }
-// }
-
-// export default connect(mapStateToProps,{
-//     followUser: followAC,
-//     unfollowUser: unfollowAC,
-//     setUsers: setUsersAC,
-//     setCurrentPage: setCurrentPageAC,
-//     setTotalUsersCount: setTotalUsersCountAC,
-//     toggleIsFetching: toggleIsFetchingAC
-// })(UsersContainer);
-
-// Вставляем { объект } mapStateToProps в hoc connect и получаем сокращение кода ч-з одинаковое название callback-функций и ActionCreator. 
-// В объекте, когда свойство и значение имеют одинаковое написание, то можно писать только свойства.
-// А ф-ция connect сама внутри себя создаёт метод mapDispatchToProps, который возвращает объект, при наличии объекта внутри себя.
-
-
-
-
-// let withRedirectToLoginUsers = HOCRedirectToLogin(UsersContainer)
-
-// export default connect(mapStateToProps, { 
-//         followUser, unfollowUser,  
-//         setCurrentPage, getUsersThunkCreator } )(withRedirectToLoginUsers);
-
-//Т.к. connect возвращает контейнерную компоненту, то её можно передать в качестве аргумента в ф-цию HOCRedirectToLogin  
-//То же самое:
-
-// export default HOCRedirectToLogin(connect(mapStateToProps, { 
-//         followUser, unfollowUser,  
-//         setCurrentPage, getUsersThunkCreator } )(UsersContainer))
 
 export default compose(
     connect(mapStateToProps, { followUser, unfollowUser, setCurrentPage, getUsersThunkCreator } ),
