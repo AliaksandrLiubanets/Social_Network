75b4128ed92d44df8b30bb7ee6aadf0d8b0e36f1
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-05-19 11:25:08 +0300

    refactor conflicts after merge with training

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,5 @@
 import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
-<<<<<<< HEAD
-import loginUserAvatar from '../../assets/loginUserAvatar.png'
-
-const Header = (props) => {
-debugger;
-  return <header className={s.header}>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6_lULuLhKRsxlcn_z3OW5mU4IV1v18fTkEg&usqp=CAU"></img>
-
-    {
-      !props.isAuth
-        ? <NavLink to={'./login'}>
-          <div className={s.authorizedUser}>
-            <div className={s.userAvatar}><img src={loginUserAvatar} /></div>
-            <div className={s.login}>Login</div>
-          </div>
-        </NavLink>
-        : <NavLink to={'./profile/' + props.userId}>
-          <div className={s.authorizedUser}>
-            <div className={s.userAvatar}><img src={props.userAvatar} /></div>
-            <div className={s.userLogin}>{props.login}</div>
-          </div>
-        </NavLink>
-    }
-
-=======
 import loginUserAvatar from '../../assets/loginUserAvatar.png';
 import userEmptyAvatar from '../../assets/userEmptyAvatar.png'
 
@@ -54,14 +29,9 @@ const Header = (props) => {
           </div>
         </NavLink>
     }
-
->>>>>>> training
   </header>
 }
 // доделать, чтобы при клике на NavLink to={'./profile' + userId} был переход на страницу профиля с залогигиным userId.
 
-<<<<<<< HEAD
-=======
 
->>>>>>> training
 export default Header;
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
 import { setAuthUserDataThunkCreator, setUserAvatar } from '../../redux/reducer-auth';
