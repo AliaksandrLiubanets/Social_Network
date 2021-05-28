f41ffc7c12842b8a3b62f67ab2da32676337295d
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-05-28 20:23:07 +0300

    Use Login.module.css for Dialogs and Profile
    errors under textarea has red color

--- src/common/formControls/Textarea.jsx
+++ src/common/formControls/Textarea.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import s from '../../components/Login/Login.module.css'
 
 export const Textarea = ({input, meta: {touched: touched, error: error}, ...props}) => {
    
     return <div>
         <textarea {...input}></textarea>
-        <div>
+        <div className={s.error}>
             {
                 touched && 
                 (error && <span>{error}</span>)
