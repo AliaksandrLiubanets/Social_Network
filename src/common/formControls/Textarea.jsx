b7968cfd574460291096c624b7368bc511d9cd4a
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-05-28 20:52:39 +0300

    Change borderColor of Textarea into red when error

--- src/common/formControls/Textarea.jsx
+++ src/common/formControls/Textarea.jsx
@@ -4,7 +4,7 @@ import s from '../../components/Login/Login.module.css'
 export const Textarea = ({input, meta: {touched: touched, error: error}, ...props}) => {
    
     return <div>
-        <textarea {...input}></textarea>
+        <textarea {...input} className={touched && error ? s.errorBorder : undefined}></textarea>
         <div className={s.error}>
             {
                 touched && 
