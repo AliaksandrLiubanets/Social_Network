bd44c3ffc2a504cd9435e34e29fed589a93ac27d
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-06-15 17:05:13 +0300

    refact setAuthUserDataThunkCreator in reducer-auth
    Use async - await instead .then

--- src/redux/reducer-auth.js
+++ src/redux/reducer-auth.js
@@ -18,13 +18,13 @@ const reducerAuth = (state = initialState, action) => {
             return {
                 ...state,
                 ...action.data
-                
+
             }
         case SET_USER_AVATAR:
             return {
                 ...state,
                 userAvatar: action.photo
-            }      
+            }
         default:
             return state;
     }
@@ -33,17 +33,17 @@ const reducerAuth = (state = initialState, action) => {
 export const setAuthUserDataAC = (userId, email, login, isAuth) => ({ type: SET_USER_DATA, data: { userId, email, login, isAuth } });
 export const setUserAvatarAC = (photo) => ({ type: SET_USER_AVATAR, photo });
 
-export const setAuthUserDataThunkCreator = () => (dispatch) => { 
-     return authAPI.getAuthData()       
-        .then(response => {             
-            if (response.data.resultCode === 0) {
-                let { id, email, login } = response.data.data;
-                dispatch(setAuthUserDataAC(id, email, login, true))
-            }
-        })
+export const setAuthUserDataThunkCreator = () => async (dispatch) => {  
+    let response = await authAPI.getAuthData()
+
+    if (response.data.resultCode === 0) {
+        let { id, email, login } = response.data.data;
+        dispatch(setAuthUserDataAC(id, email, login, true))
+    }
+
 }
 
-export const setUserAvatar = (userId) => (dispatch) => {  
+export const setUserAvatar = (userId) => (dispatch) => {
     profileAPI.getProfile(userId)
         .then(data => {
             dispatch(setUserAvatarAC(data.photos.small))   // I wait that photo will appear when I download my photo on server
@@ -51,27 +51,27 @@ export const setUserAvatar = (userId) => (dispatch) => {
 }
 
 export const login = (email, password, rememberMe) => (dispatch) => {
-   
+
     authAPI.login(email, password, rememberMe)            // make post request
         .then(response => {
-            if(response.data.resultCode === 0) {
-                dispatch(setAuthUserDataThunkCreator())   
+            if (response.data.resultCode === 0) {
+                dispatch(setAuthUserDataThunkCreator())
             } else {
-                let errorText 
-                if (response.data.messages.length > 0) {errorText = response.data.messages[0]} 
-                dispatch(stopSubmit('loginForm', {_error: errorText} ))
+                let errorText
+                if (response.data.messages.length > 0) { errorText = response.data.messages[0] }
+                dispatch(stopSubmit('loginForm', { _error: errorText }))
             }
         })
 }
 
 export const logout = () => (dispatch) => {
     authAPI.logout()
-    .then(response => {
-        if(response.data.resultCode === 0) {
-            dispatch(setAuthUserDataAC(null, null, null, false))
-        }
-    })
-} 
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setAuthUserDataAC(null, null, null, false))
+            }
+        })
+}
 
 export default reducerAuth;
 
