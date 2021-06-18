6a7617be0b108edf4a8ce19ae89896e3ba406978
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-06-18 22:03:33 +0300

    refactor action's names
    Add 'dialogs' in action's names.
    Delete comments and old actions.

--- src/redux/reducer-dialogs.js
+++ src/redux/reducer-dialogs.js
@@ -1,7 +1,5 @@
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
-const SEND_MESSAGE = 'SEND-MESSAGE';
-const ADD_SOME_AVATAR_AND_NAME = 'ADD-SOME-AVATAR-AND-NAME';
-const SET_DIALOGS_MESSAGE = 'SET_DIALOGS_MESSAGE';
+const UPDATE_NEW_MESSAGE_BODY = 'dialogs/UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'dialogs/SEND-MESSAGE';
 
 let initialState = {
         dialogsData: [
@@ -11,19 +9,7 @@ let initialState = {
             { id: 4, name: 'Sasha', message: 'Say somethink', avatar: 'https://i.pinimg.com/564x/98/b2/88/98b28840d82a8ec637642f4285a3f38a.jpg' },
             { id: 5, name: 'Victor', message: 'Dimych say: Hi', avatar: 'https://i.pinimg.com/originals/06/2b/c9/062bc9095337818bb479b69f9d807e53.jpg' },
             { id: 6, name: 'Valera', message: 'Bu-u-u', avatar: 'https://i.pinimg.com/originals/f4/05/4c/f4054c3db50a08c2f266e82b44b5ca90.jpg' },
-        ],
-        // messagesData: [
-        //     { id: 1, message: 'Dimych say: Hi' },
-        //     { id: 2, message: 'Andrey say: How is it going?' },
-        //     { id: 3, message: 'Sveta say: Hello everyone' },
-        //     { id: 4, message: 'Sasha say: Yo' },
-        //     { id: 5, message: 'Victor say: Yo' },
-        //     { id: 6, message: 'Valera Say: Yo' },
-        // ],
-        // newMessageBody: '',
-        // someAvatarAndName: { id: 7, name: 'Somebody', avatar: 'https://live.warthunder.com/style/img/no_avatar.jpg' },
-        // dialogsMessage: ''   
-
+        ]  
 }
 
 const reducerDialogs = (state = initialState, action) => {
@@ -47,20 +33,8 @@ const reducerDialogs = (state = initialState, action) => {
             return {
                 ...state,
                 dialogsData: [...state.dialogsData, newMessage]
-                // newMessageBody: ''
             }
         
-        // case ADD_SOME_AVATAR_AND_NAME:
-        //     let someAvatarAndName = state.someAvatarAndName;
-        //     state.dialogsData.push(someAvatarAndName);   
-        //     return state;
-
-        // case SET_DIALOGS_MESSAGE: 
-        //     return {
-        //         ...state,
-        //         dialogsMessage: action.message
-        //     }
-
         default:
             return state;
     }
