837f59f1a1b196a26d22af94bd93254b780be035
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-06-19 14:36:58 +0300

    refactor reducer-users: kill duplicate code
    For action.type  follow - unfollow in reducerUsers
    create function object-helpers with commom logic.

--- src/redux/reducer-users.js
+++ src/redux/reducer-users.js
@@ -1,4 +1,5 @@
 import { usersAPI } from "../api/api";
+import { updateObjectInArray } from "../common/object-helpers";
 
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
@@ -25,22 +26,24 @@ const reducerUsers = (state = initialState, action) => {
         case FOLLOW:
             return {
                 ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return { ...u, followed: true }
-                    }
-                    return u;
-                })
+                users: updateObjectInArray(state.users, 'id', action.userId, {followed: true})
+                // state.users.map(u => {
+                //     if (u.id === action.userId) {
+                //         return { ...u, followed: true }
+                //     }
+                //     return u;
+                // })
             }
         case UNFOLLOW:
             return {
                 ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return { ...u, followed: false }
-                    }
-                    return u;
-                })
+                users: updateObjectInArray(state.users, 'id', action.userId, {followed: false})
+                // state.users.map(u => {
+                //     if (u.id === action.userId) {
+                //         return { ...u, followed: false }
+                //     }
+                //     return u;
+                // })
             }
         case SET_USERS:
             return { ...state, users: action.users }
