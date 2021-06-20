b7e38f1ac8907019b6c4b26574f6c780b1c8b3cb
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-06-20 18:23:36 +0300

    refactor UsersContainer: Destructuring assignment
    in componentDidMount and onChangeClick

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,58 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setCurrentPage,   
-         getUsersThunkCreator,
-         followUser,
-         unfollowUser } from '../../redux/reducer-users';
+import {
+    setCurrentPage,
+    getUsersThunkCreator,
+    followUser,
+    unfollowUser
+} from '../../redux/reducer-users';
 import Users from '../Users/Users';
 import Preloader from '../../common/Preloader';
 
 import { compose } from 'redux';
-import { getUsers, 
-         getPageSize, 
-         getTotalUsersCount, 
-         getCurrentPage, 
-         getIsFetching, 
-         getFollowingInProgress, 
-         tenthUsers,
-         everySecondUserWithPhoto,
-         filteredWithPhoto,
-         usersWithNameDmitriy,
-         usersWithLetterD,
-         usersWithTheFirstLetterDd} from '../../redux/selectors/user-selectors';
+import {
+    getUsers,
+    getPageSize,
+    getTotalUsersCount,
+    getCurrentPage,
+    getIsFetching,
+    getFollowingInProgress,
+    tenthUsers,
+    everySecondUserWithPhoto,
+    filteredWithPhoto,
+    usersWithNameDmitriy,
+    usersWithLetterD,
+    usersWithTheFirstLetterDd
+} from '../../redux/selectors/user-selectors';
 
-class UsersContainer extends React.Component {        
-       
-    componentDidMount() {  
+class UsersContainer extends React.Component {
 
-        this.props.getUsersThunkCreator(this.props.currentPage, this.props.pageSize)
-                
-    }    
-    
-    onChangeClick = (pageNumber) => {
-
-        this.props.getUsersThunkCreator(pageNumber, this.props.pageSize)
-
-    }    
-            
-    render = () => {                    
-
-            return <> 
-            { this.props.isFetching ? <Preloader />  : null } 
-            
-            <Users   totalUsersCount={this.props.totalUsersCount} 
-                            pageSize={this.props.pageSize}
-                            currentPage={this.props.currentPage}
-                            users={this.props.users}
-                            onChangeClick={this.onChangeClick} 
-                            followUser={this.props.followUser}
-                            unfollowUser={this.props.unfollowUser}
-                            isFetching={this.props.isFetching}
-                            followingInProgress={this.props.followingInProgress}
-                             />
-                            </>
+    componentDidMount() {
+        const { currentPage, pageSize } = this.props
+        this.props.getUsersThunkCreator(currentPage, pageSize)
     }
 
+    onChangeClick = (pageNumber) => {
+        const { pageSize } = this.props
+        this.props.getUsersThunkCreator(pageNumber, pageSize)
+    }
+
+    render = () => {
+        return <>
+            {this.props.isFetching ? <Preloader /> : null}
+
+            <Users  totalUsersCount={this.props.totalUsersCount}
+                    pageSize={this.props.pageSize}
+                    currentPage={this.props.currentPage}
+                    users={this.props.users}
+                    onChangeClick={this.onChangeClick}
+                    followUser={this.props.followUser}
+                    unfollowUser={this.props.unfollowUser}
+                    isFetching={this.props.isFetching}
+                    followingInProgress={this.props.followingInProgress} />
+        </>
+    }
 }
 
 const mapStateToProps = (state) => {
@@ -66,12 +65,12 @@ const mapStateToProps = (state) => {
         pageSize: getPageSize(state),
         totalUsersCount: getTotalUsersCount(state),
         currentPage: getCurrentPage(state),
-        isFetching: getIsFetching(state),        
+        isFetching: getIsFetching(state),
         followingInProgress: getFollowingInProgress(state),
     }
 }
 
 export default compose(
-    connect(mapStateToProps, { followUser, unfollowUser, setCurrentPage, getUsersThunkCreator } )
+    connect(mapStateToProps, { followUser, unfollowUser, setCurrentPage, getUsersThunkCreator })
 )(UsersContainer)
 
